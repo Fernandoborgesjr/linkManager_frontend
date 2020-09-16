253259a60016fc7bcd89f0bd00b3d19a43313460
Fernando Borges <fernandoborgesjr@outlook.com>
2020-09-16 19:17:02 -0300

    Sign up implemented

--- src/screens/SignUp/index.js
+++ src/screens/SignUp/index.js
@@ -1,36 +1,49 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { signUp } from './SignUpActions';
 
 const SignUp = () => {
-    return (
-        <div className="container h-100 pt-5">
-            <h1> Cadastrar </h1>
-            <div className="d-flex flex-column h-100">
-                <form>
-                    <div className="form-group">
-                        <label>E-mail</label>
-                        <input type="text" className="form-control" required/>
-                    </div>
-                    <div className="form-group">
-                        <label>Senha</label>
-                        <input type="password" className="form-control" placeholder="Digite uma senha" required/>
-                    </div>
-                    <div className="form-group">
-                        <label>Confirmar Senha</label>
-                        <input type="password" className="form-control" placeholder="Confirme a senha digitada" required/>
-                    </div>
-                    <div>
-                        <button className="btn btn-primary btn-round">Cadastrar</button>
-                    </div>
-                </form>
-                <div className="container text-center fixed-bottom pb-5">
-                    <div className="text-muted">Já possui uma conta?</div>
-                    <Link to='/sign-in'>Entrar</Link>
-                </div>
-            </div>
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const data = Object.fromEntries(formData);
+    signUp(data);
+  };
+
+  return (
+    <div className="container h-100 pt-5">
+      <h1> Cadastrar </h1>
+      <div className="d-flex flex-column h-100">
+        <form onSubmit={submitHandler}>
+          <div className="form-group">
+            <label>E-mail</label>
+            <input name="email" type="text" className="form-control" required />
+          </div>
+          <div className="form-group">
+            <label>Senha</label>
+            <input name="password" type="password" className="form-control" placeholder="Digite uma senha" required />
+          </div>
+          <div className="form-group">
+            <label>Confirmar Senha</label>
+            <input name="passwordConfirmation" type="password" className="form-control" placeholder="Confirme a senha digitada" required />
+          </div>
+          <div>
+            <button className="btn btn-primary btn-round">Cadastrar</button>
+          </div>
+        </form>
+        <div className="container text-center fixed-bottom pb-5">
+          <div className="text-muted">Já possui uma conta?</div>
+          <Link to="/sign-in">Entrar</Link>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
+const mapStateToProps = (state) => {
+  return { account: state.signUp.account };
+};
 
-export default SignUp;
+export default connect(mapStateToProps, { signUp })(SignUp);
