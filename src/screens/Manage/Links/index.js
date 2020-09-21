4d7141023b233164e781d2db2b60017dc91c8bd4
Fernando Borges <fernandoborgesjr@outlook.com>
2020-09-21 19:00:46 -0300

    Listing links

--- src/screens/Manage/Links/index.js
+++ src/screens/Manage/Links/index.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Layout from '../../Layouts/Manage/index';
-const Links = () => {
+import { linkList } from '../../../actions/LinkActions'
+const Links = ({ links, linkList }) => {
+
+    useEffect(() => {
+        linkList();
+    }, [linkList]);
+
     return (
         <Layout>
             <div className="row">
@@ -11,24 +18,34 @@ const Links = () => {
                 <div className="col text-right align-self-bottom pt-2">
                     <Link to='/manage/links/create' className="btn btn-primary">Adicionar</Link>
                 </div>
-                </div>
-                <div className="pb-2 pt-2 pl-3 pr-3 d-flex flex-row justify-content-between">
-                    <div className="pr-3">
-                        <img src="https://via.placeholder.com/100" alt="Link icon" />
-                    </div>
-                    <div className="align-self-center">
-                        <span className="text-primary clearfix">Descrição</span>
-                        <span className="text-primary clearfix">URL</span>
-                    </div>
-                    <div className="ml-auto p-2 clearfix">
-                        <span>Editar</span>
-                        <span>Delete</span>
-                    </div>
-                </div>
-           
+            </div>
+
+            {links && links.length ?
+
+                links.map((link) => {
+                    return (
+                        <div className="pb-2 pt-2 pl-3 pr-3 d-flex flex-row justify-content-between">
+                            <div className="pr-3">
+                                <img src="https://via.placeholder.com/100" alt="Link icon" />
+                            </div>
+                            <div className="align-self-center">
+                                <span className="text-primary clearfix strong">{link.label}</span>
+                                <span className="text-primary clearfix">{link.url}</span>
+                            </div>
+                            <div className="ml-auto p-2 clearfix">
+                                <span>Editar</span>
+                                <span>Delete</span>
+                            </div>
+                        </div>
+                    )
+                })
+                : (<h1>Ainda nenhum link cadastrado :(</h1>)}
         </Layout>
     );
 };
 
+const mapStateToProps = (state) => {
+    return { links: state.link.links };
+};
 
-export default Links;
+export default connect(mapStateToProps, { linkList })(Links);
